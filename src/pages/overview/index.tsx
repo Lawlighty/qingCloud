b99e5574bbc685d07cf1be99ff3dcdcd952138a6
lawlighty <liyixin123>
2020-12-24 00:30:39 +0800

    总览-资源/我的信息

--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { connect, Dispatch } from 'umi';
 import styles from './index.less';
-import { Tabs, Button, notification, Card, Tooltip, Input } from 'antd';
+import { Tabs, Button, notification, Card, Tooltip, Input, Progress } from 'antd';
 import { Link } from 'umi';
 import { ReadFilled } from '@ant-design/icons';
 import { QuestionCircleFilled, SearchOutlined } from '@ant-design/icons';
 const { Meta } = Card;
+interface resourceType {
+  name: string;
+  percent: number;
+  tips: string;
+}
 const Overview: React.FC<{}> = (props) => {
   const tips = () => {
     return (
@@ -18,11 +23,50 @@ const Overview: React.FC<{}> = (props) => {
       </div>
     );
   };
+  const tipsTick = () => {
+    return <div className="font_12_impor">各优惠券使用详情请前往优惠券列表查看</div>;
+  };
   // ip定位资源
   const onPressEnter = (e) => {
     console.log('输入的ip', e);
     console.log('输入的ip', e.target.value);
   };
+  // 资源配额使用情况
+  // 计算
+  const calDemo = [
+    {
+      name: '基础型主机 (个)',
+      percent: 10,
+      tips: '1/10 个',
+    },
+    {
+      name: '基础型主机 (个)',
+      percent: 20,
+      tips: '1/10 个',
+    },
+    {
+      name: '基础型主机 (个)',
+      percent: 30,
+      tips: '1/10 个',
+    },
+    {
+      name: '基础型主机 (个)',
+      percent: 40,
+      tips: '1/10 个',
+    },
+    {
+      name: '基础型主机 (个)',
+      percent: 50,
+      tips: '1/10 个',
+    },
+    {
+      name: '基础型主机 (个)',
+      percent: 60,
+      tips: '1/10 个',
+    },
+  ];
+  const [cal, setCal] = useState<resourceType[]>(calDemo);
+  // const [cal, setCal] = useState<resourceType[]>(calDemo);
   return (
     <PageContainer>
       <div className="min_w_800 font_12">
@@ -78,6 +122,7 @@ const Overview: React.FC<{}> = (props) => {
             <div className={styles.overview_quotas}>
               <div className={styles.quotas_title}>
                 <div>资源配额使用情况</div>
+                {/* tips */}
                 <div className={styles.alert_info}>
                   你离开始使用仅差一步：立即完成认证，即可享受相应数量的资源配额和测试费。
                   <a href="/account/profile/advanced/" data-permalink="">
@@ -85,10 +130,72 @@ const Overview: React.FC<{}> = (props) => {
                   </a>
                 </div>
               </div>
+              <div className="flex_start">
+                <div className="flex_1 margin_r_20">
+                  <div className={styles.quota_inner}>
+                    <div className={styles.quota_title}>计算</div>
+                    {cal.map((item) => (
+                      <div className={styles.quota_usage}>
+                        <div className={styles.quota_usage_name}>{item.name}</div>
+                        <Tooltip title={item.tips}>
+                          <Progress percent={item.percent} showInfo={false} />
+                        </Tooltip>
+                      </div>
+                    ))}
+                  </div>
+                  <div className={styles.quota_inner}>
+                    <div className={styles.quota_title}>存储</div>
+                    {cal.map((item) => (
+                      <div className={styles.quota_usage}>
+                        <div className={styles.quota_usage_name}>{item.name}</div>
+                        <Tooltip title={item.tips}>
+                          <Progress percent={item.percent} showInfo={false} />
+                        </Tooltip>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div className="flex_1 margin_r_20">
+                  <div className={styles.quota_inner}>
+                    <div className={styles.quota_title}>网络与 CDN</div>
+                    {cal.map((item) => (
+                      <div className={styles.quota_usage}>
+                        <div className={styles.quota_usage_name}>{item.name}</div>
+                        <Tooltip title={item.tips}>
+                          <Progress percent={item.percent} showInfo={false} />
+                        </Tooltip>
+                      </div>
+                    ))}
+                  </div>
+                  <div className={styles.quota_inner}>
+                    <div className={styles.quota_title}>安全</div>
+                    {cal.map((item) => (
+                      <div className={styles.quota_usage}>
+                        <div className={styles.quota_usage_name}>{item.name}</div>
+                        <Tooltip title={item.tips}>
+                          <Progress percent={item.percent} showInfo={false} />
+                        </Tooltip>
+                      </div>
+                    ))}
+                  </div>
+                  <div className={styles.quota_inner}>
+                    <div className={styles.quota_title}>容器</div>
+                    {cal.map((item) => (
+                      <div className={styles.quota_usage}>
+                        <div className={styles.quota_usage_name}>{item.name}</div>
+                        <Tooltip title={item.tips}>
+                          <Progress percent={item.percent} showInfo={false} />
+                        </Tooltip>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           {/* // 右侧 */}
           <div className={styles.overview_right}>
+            {/* 广告 */}
             <div className="overview-promotions">
               <a
                 href="https://www.qingcloud.com/promotion202010/#security"
@@ -100,6 +207,48 @@ const Overview: React.FC<{}> = (props) => {
                   src="https://s3.qingcloud.com/files/2020/08/31171608194518_.pic_.png"
                 />
               </a>
+            </div>
+
+            {/* 账户信息 */}
+            <div className={styles.overview_consumptions}>
+              <div className={styles.overview_consumptions_inner}>
+                <div className={styles.overview_card_title}>
+                  <div className="flex_1">Hi, 13616859570</div>
+
+                  <a className={styles.btn_link} href="/finance/statistic/">
+                    消费统计
+                  </a>
+                </div>
+                <div className={styles.balance}>
+                  <div className={styles.balance_inner}>
+                    <div className={styles.balance_title}>账户余额</div>
+                    <div className={styles.balance_total}>
+                      <span className={styles.currency}>¥</span>&nbsp;0.00
+                    </div>
+                    <div className={styles.balance_coupons}>
+                      <div className={styles.items_title}>优惠券</div>
+                      <Tooltip title={tipsTick}>
+                        <a className={styles.items_value} href="/finance/coupons/">
+                          ¥2.00
+                        </a>
+                      </Tooltip>
+                    </div>
+                    <div className={styles.balance_coupons}>
+                      <div className={styles.items_title}>预计支持</div>
+                      <div className={` ${styles.items_value} ${styles.estimated_value}`}>
+                        <div className={styles.estimated_inner}>
+                          <span className={styles.estimated_inner_success}>当前账户无预估</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.balance_charge}>
+                      <a className={styles.btn_primary} href="/finance/wallet/" data-permalink="">
+                        立即充值
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
